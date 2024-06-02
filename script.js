a16833083aec1395d58d5c958084d047eeca38b1
ThatFrostyy <juniorjegej@gmail.com>
2024-06-02 11:54:50 +0200

    Update 2

---
+++ script.js
@@ -0,0 +1,14 @@
+const greetingElement = document.querySelector(".greeting");
+greetingElement.textContent = getGreeting();
+
+function getGreeting() {
+    const currentTime = new Date().getHours();
+
+    if (currentTime < 12) {
+        return "🌇 Good morning, stalker.";
+    } else if (currentTime < 18) {
+        return "☀️ Good afternoon, stalker.";
+    } else {
+        return "🌃 Good evening, stalker.";
+    }
+}
