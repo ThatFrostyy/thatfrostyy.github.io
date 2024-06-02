dff1a6e0ae6b11d75a80e23bee92b7624076872c
ThatFrostyy <juniorjegej@gmail.com>
2024-06-02 12:58:38 +0200

    Update 4

--- script.js
+++ script.js
@@ -13,6 +13,16 @@ function getGreeting() {
     }
 }
 
+function toggleContent(contentId) {
+    const allContent = document.querySelectorAll('[id$="-content"]');
+    allContent.forEach(content => {
+        content.classList.add('hidden');
+    });
+
+    const selectedContent = document.getElementById(contentId);
+    selectedContent.classList.remove('hidden');
+}
+
 function openDiscordServer() {
     const discordServerLink = 'https://discord.gg/JuZMZGMRMd';
     window.open(discordServerLink, '_blank');
